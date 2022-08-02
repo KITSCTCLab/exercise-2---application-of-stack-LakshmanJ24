0e75c7dfb7daf54c9d5610c09ef6cc56ed4bab19
LakshmanJ24 <100009458+LakshmanJ24@users.noreply.github.com>
2022-08-02 10:06:53 +0530

    17th try

--- Main.py
+++ Main.py
@@ -4,55 +4,55 @@ class Evaluate:
     self.size = size
     self.lst = [None]*size
 
- def isEmpty(self):
+  def isEmpty(self):
     if self.top == -1:
       return 1
     else :
       return 0
    
- def is_full(self):
+  def is_full(self):
     if self.top == (self.size - 1):
       return 1
     else :
       return 0
 
- def pop(self):
+  def pop(self):
     if not self.isEmpty():
       t=self.lst[self.top]
       del self.lst[self.top]
       self.top-=1
       return t
 
- def push(self, operand):
-  if not self.is_full():
-    self.top+=1
-    self.lst[self.top]=operand
+  def push(self, operand):
+    if not self.is_full():
+      self.top+=1
+      self.lst[self.top]=operand
 
- def validate_postfix_expression(self, expression):
-  c1=0
-  c2=0
-  for i in expression:
-    if i.isdigit():
-      c1+=1
+  def validate_postfix_expression(self, expression):
+    c1=0
+    c2=0
+    for i in expression:
+      if i.isdigit():
+        c1+=1
+      else:
+        c2+=1
+    if c1>c2 and expression[0].isdigit() and expression[1].isdigit():
+      return 1
     else:
-      c2+=1
-  if c1>c2 and expression[0].isdigit() and expression[1].isdigit():
-    return 1
-  else:
-    return 0
+      return 0
 
- def evaluate_postfix_expression(self, expression):
-  for i in expression:
-    if i.isdigit():
-      self.push(i)
+  def evaluate_postfix_expression(self, expression):
+    for i in expression:
+      if i.isdigit():
+        self.push(i)
+      else:
+        v1 = self.pop()
+        v2 = self.pop()
+    if i=='/':
+      self.push(str(eval(v2 + i*2 + v1)))
     else:
-      v1 = self.pop()
-      v2 = self.pop()
-  if i=='/':
-    self.push(str(eval(v2 + i*2 + v1)))
-  else:
-    self.push(str(eval(v2 + i + v1)))
-    return self.pop()
+      self.push(str(eval(v2 + i + v1)))
+      return self.pop()
 
 # Do not change the following code
 postfix_expression = input()  # Read postfix expression
